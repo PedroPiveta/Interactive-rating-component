b836a4d6020ace617a7a08323d13f9d9c05ee263
PedroPiveta <ppivetabarrotti208@gmail.com>
2023-02-06 17:54:32 -0300

    completed the challenge

---
+++ README.md
@@ -0,0 +1,5 @@
+# Frontend Challenge from Frontend Mentor
+
+[Challenge link](https://www.frontendmentor.io/challenges/interactive-rating-component-koxpeBUmI)
+
+## You can check my solution [here](https://interativeratingcomponent.netlify.app/)
--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 addEventListener("DOMContentLoaded", () => {
     const checkboxes = document.querySelectorAll('span.number')
+    const form = document.querySelector('form')
+    const main = document.querySelector('main')
+    const thankScreen = document.querySelector('.postRate')
     let count = 0
+    const rating = document.querySelector('.ratingText')
+
     checkboxes.forEach((checkbox) => {
         checkbox.addEventListener('click', (e) => {
             //change checkbox class name
@@ -13,6 +18,14 @@ addEventListener("DOMContentLoaded", () => {
             }
             console.log(count)
         })
+    })
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        console.log('submitted')
+        main.style.display = 'none'
+        thankScreen.style.display = 'block'
+        rating.innerHTML = `You selected ${count} out of 5`
     })
 })
 
